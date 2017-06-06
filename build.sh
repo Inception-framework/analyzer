08cdcbaa071aefa8fbca7041ec3889b97a932ce4
Corteggiani <nassim.corteggiani@maximintegrated.com>
2017-06-06 08:45:57 +0200

    Update build.sh to support Inception installer

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 CXXFLAGS="-g3 -O0 -fexceptions" CFLAGS="-g3 -O0" ./configure \
             --enable-cxx11 \
             --with-stp=../tools/stp/build \
             --enable-posix-runtime \
-            --with-llvmsrc=/home/giovanni/phd/Inception/tools/llvm/llvm3.4 \
-            --with-llvmobj=/home/giovanni/phd/Inception/tools/llvm/llvm3.4 \
-            --with-llvmcc=/home/giovanni/phd/Inception/tools/llvm/llvm3.4/build/bin/clang \
-            --with-llvmcxx=/home/giovanni/phd/Inception/tools/llvm/llvm3.4/build/bin/clang++ \
-            --with-uclibc=/home/giovanni/phd/Inception/tools/klee-uclibc \
+            --with-llvmsrc=$DIR/../tools/llvm/llvm3.4 \
+            --with-llvmobj=$DIR/../tools/llvm/llvm3.4 \
+            --with-llvmcc=$DIR/../tools/llvm/llvm3.4/Debug+Asserts/bin/clang \
+            --with-llvmcxx=$DIR/../tools/llvm/llvm3.4/Debug+Asserts/bin/clang++ \
+            --with-uclibc=$DIR/../tools/klee-uclibc \
             --with-runtime='Debug+Asserts'
 
 if [ $? != 0 ]; then
@@ -31,4 +33,4 @@ echo "Press any key to install klee"
 read i
 
 sudo -S rm /usr/bin/klee
-sudo -S ln -s /home/giovanni/phd/Inception/Analyzer/Debug+Asserts/bin/klee /usr/bin/klee
+sudo -S ln -s $DIR/Debug+Asserts/bin/klee /usr/bin/klee
