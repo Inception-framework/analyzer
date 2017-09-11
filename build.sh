a146e77290583d15ea282c8f50e2ef69696ae0c7
Corteggiani <nassim.corteggiani@maximintegrated.com>
2017-09-11 09:10:43 +0200

    Adding multi build support : Release/Debug

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 make clean
 
 LLVM_VERSION=3.6
+MODE=DEBUG
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #./autoconf/AutoRegen.sh
 ./autoconf/AutoRegen.sh $DIR/../tools/llvm/llvm3.6/
 
+if [ $MODE = DEBUG ]; then
 CXXFLAGS="-g3 -O0 -fexceptions" CFLAGS="-g3 -O0" ./configure \
             --enable-cxx11 \
             --with-stp=$DIR/../tools/stp/ \
@@ -19,6 +21,19 @@ CXXFLAGS="-g3 -O0 -fexceptions" CFLAGS="-g3 -O0" ./configure \
             --with-uclibc=$DIR/../tools/klee-uclibc \
             --with-runtime='Debug+Asserts' \
 	    --with-llvm-build-mode='Debug+Asserts'
+else
+CXXFLAGS="-O3 -fexceptions" CFLAGS="-O3" ./configure \
+            --enable-cxx11 \
+            --with-stp=$DIR/../tools/stp/ \
+            --enable-posix-runtime \
+            --with-llvmsrc=$DIR/../tools/llvm/llvm$LLVM_VERSION \
+            --with-llvmobj=$DIR/../tools/llvm/build_release \
+            --with-llvmcc=$DIR/../tools/llvm/build_release/Release/bin/clang \
+            --with-llvmcxx=$DIR/../tools/llvm/build_release/Release/bin/clang++ \
+            --with-uclibc=$DIR/../tools/klee-uclibc \
+            --with-runtime='Release' \
+            --with-llvm-build-mode='Release'
+fi
 
 if [ $? != 0 ]; then
         echo "FAIL to configure Klee !"
